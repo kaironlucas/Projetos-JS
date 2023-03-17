781bb69d08b1088a710ff5a6fc3fecf289c17b4f
kaironlucas <noriakxp@gmail.com>
2023-03-17 15:57:30 -0300

    fi

---
+++ lampada/script.js
@@ -0,0 +1,7 @@
+function ligarLampada() {
+	document.getElementById("lamp").src = "./imagens/ligada.jpg";
+}
+
+function desligarLampada() {
+	document.getElementById("lamp").src = "./imagens/desligada.jpg";
+}
